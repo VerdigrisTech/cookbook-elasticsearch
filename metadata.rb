28312a3992eaa4f50220f1e9d9f97292df2c648b
Andrew Jo <andrewjo@gmail.com>
2014-12-31 11:35:35 -0800

    ElasticSearch initial cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'cookbook-elasticsearch'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures cookbook-elasticsearch'
+long_description 'Installs/Configures cookbook-elasticsearch'
+version          '0.1.0'
+
