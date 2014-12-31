ea75024759f9f5fe5d7b68567a6d17bc940f39fe
Andrew Jo <andrewjo@gmail.com>
2014-12-31 12:25:45 -0800

    Read from README.md for long description

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,8 @@
 maintainer       "Andrew Jo"
 maintainer_email "andrew@verdigris.co"
 license          "BSD"
-description      'Installs/Configures elasticsearch'
-long_description 'Installs/Configures elasticsearch'
-version          '0.1.0'
+description      "Installs/Configures elasticsearch"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
 
 depends "java"
