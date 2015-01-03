d7a0fa927192e8d97bbf0ae45499794b420dfc21
Andrew Jo <andrewjo@gmail.com>
2015-01-03 13:48:29 -0800

    Add name attribute to metadata

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,7 @@
 maintainer       "Andrew Jo"
 maintainer_email "andrew@verdigris.co"
 license          "BSD"
+name             "elasticsearch"
 description      "Installs/Configures elasticsearch"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
