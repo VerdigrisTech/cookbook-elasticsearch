eb4ed9bb3d36231674f774e13d3cb34085a236e2
Andrew Jo <andrewjo@gmail.com>
2015-01-03 13:26:37 -0800

    Init.d script generation

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,18 @@
 #-*- encoding : utf-8 -8-
 
+# Environment defaults
 template "etc.default" do
   path    "/etc/default/elasticsearch"
   source  "etc.default.erb"
   owner   node[:elasticsearch][:user]
   group   node[:elasticsearch][:user]
   mode    0755
+end
+
+# Init.d script
+template "etc.init.d" do
+  path   "/etc/init.d/elasticsearch"
+  source "etc.init.d.erb"
+  owner 'root'
+  mode 0755
 end
