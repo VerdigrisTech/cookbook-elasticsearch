236613bd336d8a14890879f6afef19e1770bbdf3
Andrew Jo <andrewjo@gmail.com>
2015-01-02 16:40:57 -0800

    Add default environment variable template

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,9 @@
-#
-# Cookbook Name:: cookbook-elasticsearch
-# Recipe:: default
-#
-# Copyright (c) 2014 The Authors, All Rights Reserved.
+#-*- encoding : utf-8 -8-
+
+template "etc.default" do
+  path    "/etc/default/elasticsearch"
+  source  "etc.default.erb"
+  owner   node[:elasticsearch][:user]
+  group   node[:elasticsearch][:user]
+  mode    0755
+end
